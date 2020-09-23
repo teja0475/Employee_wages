a0b37782fe9b38cee942f1a445834b3558c8ce4c
Tejeswar V <vtejaswar@gmail.com>
2020-09-23 23:57:13 +0530

    Added Case in a function and find the monthly wage at a time

--- Employee_Wages.sh
+++ Employee_Wages.sh
@@ -2,6 +2,7 @@
 
 echo "Welcome to employee wage computation program"
 read -p "Max working days(value be >=20): " max_days
+
 full_time=2
 half_time=1
 absent=0
@@ -10,37 +11,57 @@ full_time_hours=8
 half_time_hours=4
 total_working_days=0
 total_emp_hour=0
+emp_working_days=20
+
+function empWorkHour()
+{
+
+case  $1 in
+       1)emp_work_hour=4;;
+       2)emp_work_hour=8;;
+             *)emp_work_hour=0;;
+
+esac
+
+echo $emp_work_hour
+}
 
 
 function dailyEmpWage()
 {
 	emp_work_hour=$1
-	daily_emp_wage=$(( $rate_per_hour * $emp_work_hours ))
+	daily_emp_wage=$(( $rate_per_hour * $emp_work_hour ))
 	echo $daily_emp_wage
 }
 
 
-while (( $total_working_days < $max_days ))
+while (( $total_working_days <= $max_days ))
 do
 
 	((total_working_days++))
-	check=$((RANDOM%3))
-	if [[ $check == $full_time ]]
-	then
-		emp_work_hours=$full_time_hours
-	elif [[ $check == $half_time ]]
-	then
-		emp_work_hours=$half_time_hours
+        check=$((RANDOM%3))
+        if [[ $check -eq 2 ]]
+        then
+                get_hours=2
 
-	else
-		emp_work_hours=$absent
+        elif [[ $check -eq 1 ]]
+        then
+                get_hours=1
+        else
+                get_hours=0
 
-	fi
+        fi
+
+	emp_hours="$( empWorkHour $get_hours )"
+	total_emp_hours=$(( $total_emp_hours + $emp_hours ))
+	emp_daily_wage="$( dailyEmpWage $emp_hours )"
+	monthly_emp_wage=$(($monthly_emp_wage + $emp_daily_wage))
 
 
 
-emp_daily_wage="$( dailyEmpWage $emp_work_hours )"
-echo $emp_daily_wage
+
+echo "$emp_daily_wage---------->$monthly_emp_wage"
 
 done
+
 
