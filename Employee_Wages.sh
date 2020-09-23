92d868ffd9b5a7f809ef8fa2262e61944be5a388
Tejeswar V <vtejaswar@gmail.com>
2020-09-23 17:50:14 +0530

    Added Code for daily wage

--- Employee_Wages.sh
+++ Employee_Wages.sh
@@ -1,18 +1,46 @@
 #!/bin/bash
-echo "Welcome to employee wage computation program"
 
+echo "Welcome to employee wage computation program"
+read -p "Max working days(value be >=20): " max_days
 full_time=2
 half_time=1
 absent=0
+rate_per_hour=20
+full_time_hours=8
+half_time_hours=4
+total_working_days=0
+total_emp_hour=0
 
-check=$((RANDOM%3))
 
-if [[ $check == $full_time ]]
-then
-	echo "Employee is present full time"
-elif [[ $check == $half_time ]]
-then
-	echo "Employee is present half time"
-else
-	echo "Employee is absent"
-fi
+function dailyEmpWage()
+{
+	emp_work_hour=$1
+	daily_emp_wage=$(( $rate_per_hour * $emp_work_hours ))
+	echo $daily_emp_wage
+}
+
+
+while (( $total_working_days < $max_days ))
+do
+
+	((total_working_days++))
+	check=$((RANDOM%3))
+	if [[ $check == $full_time ]]
+	then
+		emp_work_hours=$full_time_hours
+	elif [[ $check == $half_time ]]
+	then
+		emp_work_hours=$half_time_hours
+
+	else
+		emp_work_hours=$absent
+
+	fi
+
+
+
+emp_daily_wage="$( dailyEmpWage $emp_work_hours )"
+echo $emp_daily_wage
+
+done
+
