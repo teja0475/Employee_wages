82b33ee879a7ec35a90885cb72a72ac5fb50db0e
Tejeswar V <vtejaswar@gmail.com>
2020-09-23 16:43:11 +0530

    Added variables full time and half_time

--- Employee_Wages.sh
+++ Employee_Wages.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 echo "Welcome to employee wage computation program"
 
+full_time=2
+half_time=1
+absent=0
+
 check=$((RANDOM%3))
 
-if [[ $check == 2 ]]
+if [[ $check == $full_time ]]
 then
 	echo "Employee is present full time"
-elif [[ $check == 1 ]]
+elif [[ $check == $half_time ]]
 then
 	echo "Employee is present half time"
 else
